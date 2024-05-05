e3109040b16cb25fc6fef5fca6668e9dcfed3cd8
NanLiqwq <132761860+NanLiqwq@users.noreply.github.com>
2024-05-05 21:20:18 +0800

    Add files via upload

---
+++ Features/StyleChanger.cpp
@@ -0,0 +1,24 @@
+#include "StyleChanger.h"
+
+namespace StyleChanger 
+{
+	void UpdateSkin(int Skin) noexcept
+	{
+		switch (Skin) {
+		case 0:
+			Styles::Style_AimStar();
+			break;
+		case 1:
+			Styles::NiggaLose();
+			break;
+		case 2:
+			Styles::aimwaste();
+			break;
+		case 3:
+			Styles::sugar();
+			break;
+		default:
+			break;
+		}
+	}
+}
